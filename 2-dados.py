99f290207c8f0c70a1d45a0b264282bcc9e17d50
thiago-carneiro-sheep <thiagosheep90@gmail.com>
2026-06-28 01:23:58 -0300

    04

---
+++ 2-dados.py
@@ -0,0 +1,19 @@
+#pythonflix
+
+name = "Interestellar"
+year = 2014
+note = 8.6
+plan = "Prime Video"    
+planIncluded= False
+print(name)
+print(year)
+print(note)
+print(plan)
+print(planIncluded)
+
+print("O filme {} foi lançado em {} e tem nota {}".format(name, year, note))
+print(type(name))
+print(type(year))
+print(type(note))
+print(type(plan))
+print(type(planIncluded))
